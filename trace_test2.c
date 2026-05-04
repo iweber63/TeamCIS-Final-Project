039139d3a4ddb55761d820fb19afae81bbb3c217
coleed-stack <coleed65@gmail.com>
2026-05-04 18:41:55 -0500

    Added pipe functionality to populate more system calls

--- trace_test2.c
+++ trace_test2.c
@@ -7,18 +7,29 @@ int
 main(void)
 {
     // Trace fork and write - tracing multiple @ once
-    trace((1 << SYS_fork) | (1 << SYS_write));
+    trace((1 << SYS_fork) | (1 << SYS_write) | (1 << SYS_pipe) | (1 << SYS_read) | (1 << SYS_close));
+    
+    int p[2];
+    char buffer[12];
+    pipe(p);
 
     int pid = fork();
 
     if(pid == 0){
         // child process
-        write(1, "child running\n", 14);
+        close(p[1]);
+        read(p[0], buf, 12);
+        
+        write(1, buf, 12);   //printing received data
+        
+        close(p[0]);
         exit();
     } else {
         // parent process
-        wait();
-        write(1, "parent done\n", 12);
+        close(p[0]);
+        write(p[1], "Hello World", 12);    //sending data
+        close(p[1]);
+        wait(0);
     }
 
     exit();
